794ddd9284376765c4dd6041f6ee29a8d0e5bffc
Qudais Ashraf <147395120+QudaisAshraf@users.noreply.github.com>
2024-10-27 15:08:37 +0500

    Update4 test.py

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print('Wassup Buddy, This is Qudais working with github and jenkins simultaneously.')
 print('This is another line to showcase auto build using jenkins.')
+print('This part is being wrting to test email notifcations on build failed and then rectify.')
