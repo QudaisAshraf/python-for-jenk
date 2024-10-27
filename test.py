4810e3435d1b52eea8a0f3d661097bccaadd20a5
Qudais Ashraf <147395120+QudaisAshraf@users.noreply.github.com>
2024-10-27 15:00:57 +0500

    Update2 test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-print'Wassup Buddy, This is Qudais working with github and jenkins simultaneously.')
+print('Wassup Buddy, This is Qudais working with github and jenkins simultaneously.')
 print('This is another line to showcase auto build using jenkins.')
